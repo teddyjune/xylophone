44c998e44d6f0d688f34e86e983678f8bd475bee
yeonjoo.lee <teddyjune@naver.com>
2022-12-28 17:23:09 +0900

    feat: soundPool 사용/전반에 사운드 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,8 +21,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  //사운드를 들여오는 코드
+  Soundpool pool = Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
+
+  List<int> _soundIds = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    initSoundPool();
+  }
+
+  // 사운드가 들어오는 데 시간이 걸리는 걸 해결하는 코드
+  Future<void> initSoundPool() async {
+    int soundId =
+        await rootBundle.load('assets/do1.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+    soundId = await rootBundle.load('assets/re.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle.load('assets/mi.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle.load('assets/fa.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId =
+        await rootBundle.load('assets/sol.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle.load('assets/la.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId = await rootBundle.load('assets/si.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    soundId =
+        await rootBundle.load('assets/do2.wav').then((ByteData soundData) {
+      return pool.load(soundData);
+    });
+    _soundIds.add(soundId);
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,57 +97,65 @@ class XylophoneApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('실로폰'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: gunban('도', Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: gunban('레', Colors.orange),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: gunban('미', Colors.yellow),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: gunban('파', Colors.green),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48.0),
-            child: gunban('솔', Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 56.0),
-            child: gunban('라', Colors.grey),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: gunban('시', Colors.purple),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 72.0),
-            child: gunban('도', Colors.black),
-          ),
-        ],
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: gunban('도', Colors.red, _soundIds[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: gunban('레', Colors.orange, _soundIds[1]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: gunban('미', Colors.yellow, _soundIds[2]),
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 40.0),
+                    child: gunban('파', Colors.green, _soundIds[3])),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48.0),
+                  child: gunban('솔', Colors.blue, _soundIds[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
+                  child: gunban('라', Colors.grey, _soundIds[5]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: gunban('시', Colors.purple, _soundIds[6]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: gunban('도', Colors.black, _soundIds[7]),
+                ),
+              ],
+            ),
     );
   }
 
-  Widget gunban(String text, Color color) {
-    return Container(
-      width: 50,
-      height: double.infinity,
-      color: color,
-      child: Center(
-          child: Text(
-        text,
-        style:
-            const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      )),
+  Widget gunban(String text, Color color, int soundId) {
+    return GestureDetector(
+      onTap: () {
+        if (soundId != null) {
+          pool.play(soundId);
+        }
+      },
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+            child: Text(
+          text,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        )),
+      ),
     );
   }
 }
---
+++ lib/sound_pool.dart
@@ -0,0 +1,234 @@
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:soundpool/soundpool.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(MaterialApp(home: SoundpoolInitializer()));
+// }
+//
+// class SoundpoolInitializer extends StatefulWidget {
+//   @override
+//   _SoundpoolInitializerState createState() => _SoundpoolInitializerState();
+// }
+//
+// class _SoundpoolInitializerState extends State<SoundpoolInitializer> {
+//   Soundpool? _pool;
+//   SoundpoolOptions _soundpoolOptions = const SoundpoolOptions();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (!kIsWeb) {
+//       _initPool(_soundpoolOptions);
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     if (_pool == null) {
+//       return Material(
+//         child: Center(
+//           child: ElevatedButton(
+//             onPressed: () => _initPool(_soundpoolOptions),
+//             child: const Text("Init Soundpool"),
+//           ),
+//         ),
+//       );
+//     } else {
+//       return SimpleApp(
+//         pool: _pool!,
+//         onOptionsChange: _initPool,
+//       );
+//     }
+//   }
+//
+//   void _initPool(SoundpoolOptions soundpoolOptions) {
+//     _pool?.dispose();
+//     setState(() {
+//       _soundpoolOptions = soundpoolOptions;
+//       _pool = Soundpool.fromOptions(options: _soundpoolOptions);
+//       print('pool updated: $_pool');
+//     });
+//   }
+// }
+//
+// class SimpleApp extends StatefulWidget {
+//   final Soundpool pool;
+//   final ValueSetter<SoundpoolOptions> onOptionsChange;
+//
+//   SimpleApp({Key? key, required this.pool, required this.onOptionsChange})
+//       : super(key: key);
+//
+//   @override
+//   _SimpleAppState createState() => _SimpleAppState();
+// }
+//
+// class _SimpleAppState extends State<SimpleApp> {
+//   int? _alarmSoundStreamId;
+//   int _cheeringStreamId = -1;
+//
+//   String get _cheeringUrl => kIsWeb
+//       ? '/c-c-1.mp3'
+//       : 'https://raw.githubusercontent.com/ukasz123/soundpool/feature/web_support/example/web/c-c-1.mp3';
+//
+//   Soundpool get _soundpool => widget.pool;
+//
+//   void initState() {
+//     super.initState();
+//
+//     _loadSounds();
+//   }
+//
+//   void _loadSounds() {
+//     _soundId = _loadSound();
+//     _cheeringId = _loadCheering();
+//   }
+//
+//   @override
+//   void didUpdateWidget(SimpleApp oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//     if (oldWidget.pool != widget.pool) {
+//       _loadSounds();
+//     }
+//   }
+//
+//   double _volume = 1.0;
+//   double _rate = 1.0;
+//   late Future<int> _soundId;
+//   late Future<int> _cheeringId;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: [
+//           IconButton(
+//               onPressed: () async {
+//                 final newOptions = await Navigator.of(context).push<
+//                         SoundpoolOptions>(
+//                     MaterialPageRoute(builder: (context) => PlatformOptions()));
+//                 if (newOptions != null) {
+//                   widget.onOptionsChange(newOptions);
+//                 }
+//               },
+//               icon: const Icon(
+//                 Icons.access_alarms,
+//               ))
+//         ],
+//       ),
+//       body: Center(
+//         child: SizedBox(?
+//           width: kIsWeb ? 450 : double.infinity,
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               const Text('Rolling dices'),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: _playSound,
+//                     child: const Text("Play"),
+//                   ),
+//                   const SizedBox(width: 8),
+//                   ElevatedButton(
+//                     onPressed: _pauseSound,
+//                     child: const Text("Pause"),
+//                   ),
+//                   const SizedBox(width: 8),
+//                   ElevatedButton(
+//                     onPressed: _stopSound,
+//                     child: const Text("Stop"),
+//                   ),
+//                 ],
+//               ),
+//               const SizedBox(height: 8),
+//               ElevatedButton(
+//                 onPressed: _playCheering,
+//                 child: const Text("Play cheering"),
+//               ),
+//               const SizedBox(height: 4),
+//               const Text('Set rate '),
+//               Row(children: [
+//                 Expanded(
+//                   child: Slider.adaptive(
+//                     min: 0.5,
+//                     max: 2.0,
+//                     value: _rate,
+//                     onChanged: (newRate) {
+//                       setState(() {
+//                         _rate = newRate;
+//                       });
+//                       _updateCheeringRate();
+//                     },
+//                   ),
+//                 ),
+//                 Text('${_rate.toStringAsFixed(3)}'),
+//               ]),
+//               const SizedBox(height: 8.0),
+//               const Text('Volume'),
+//               Slider.adaptive(
+//                   value: _volume,
+//                   onChanged: (newVolume) {
+//                     setState(() {
+//                       _volume = newVolume;
+//                     });
+//                     _updateVolume(newVolume);
+//                   }),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Future<int> _loadSound() async {
+//     var asset = await rootBundle.load("sounds/do-you-like-it.wav");
+//     return await _soundpool.load(asset);
+//   }
+//
+//   Future<int> _loadCheering() async {
+//     return await _soundpool.loadUri(_cheeringUrl);
+//   }
+//
+//   Future<void> _playSound() async {
+//     var _alarmSound = await _soundId;
+//     _alarmSoundStreamId = await _soundpool.play(_alarmSound);
+//   }
+//
+//   Future<void> _pauseSound() async {
+//     if (_alarmSoundStreamId != null) {
+//       await _soundpool.pause(_alarmSoundStreamId!);
+//     }
+//   }
+//
+//   Future<void> _stopSound() async {
+//     if (_alarmSoundStreamId != null) {
+//       await _soundpool.stop(_alarmSoundStreamId!);
+//     }
+//   }
+//
+//   Future<void> _playCheering() async {
+//     var _sound = await _cheeringId;
+//     _cheeringStreamId = await _soundpool.play(
+//       _sound,
+//       rate: _rate,
+//     );
+//   }
+//
+//   Future<void> _updateCheeringRate() async {
+//     if (_cheeringStreamId > 0) {
+//       await _soundpool.setRate(
+//           streamId: _cheeringStreamId, playbackRate: _rate);
+//     }
+//   }
+//
+//   Future<void> _updateVolume(newVolume) async {
+//     // if (_alarmSound >= 0){
+//     var _cheeringSound = await _cheeringId;
+//     _soundpool.setVolume(soundId: _cheeringSound, volume: newVolume);
+//     // }
+//   }
+// }
